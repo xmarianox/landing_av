02205a92c92ed597debe0d33a6553ddf8dcd957e
Mariano <xsin.crueldadx@gmail.com>
2014-07-21 20:19:44 -0300

    Ajustes arrow draggable

--- index.php
+++ index.php
@@ -325,10 +325,7 @@
 
 				<div class="resultado" >
 
-					<div class="content_arrow">
-						<a style="cursor: pointer" id="arrow-l" class="arrow_resul left"><i class="fa fa-chevron-left"></i></a>
-						<a style="cursor: pointer" id="arrow-r" class="arrow_resul right"><i class="fa fa-chevron-right"></i></a>
-					</div>
+					<span class="content_arrow"></span>
 
 					<div class="content_mapa">
 						<div class="map-canvas" id="map-canvas">							
